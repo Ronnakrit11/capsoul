7030e4335f4f2c6bfaf5e4916fbaa8250ba0c460
nookteanook111 <vnfpower.finance@gmail.com>
2025-04-08 16:59:06 +0700

    done

--- components/PriceFormatter.tsx
+++ components/PriceFormatter.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const PriceFormatter = ({ amount, className }: Props) => {
   const formattedPrice = new Number(amount).toLocaleString("en-US", {
-    currency: "USD",
+    currency: "THB",
     style: "currency",
     minimumFractionDigits: 2,
   });
