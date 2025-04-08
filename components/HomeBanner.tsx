6fd579f0ac9bf04f08d6797a925748c2dc69cae3
nookteanook111 <vnfpower.finance@gmail.com>
2025-04-08 15:44:15 +0700

    done

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -5,7 +5,7 @@ const HomeBanner = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-5">
       <Title className="text-3xl md:text-4xl uppercase font-bold text-center">
-        Best Bedding Collection
+        Best Coffee Capsule Collection
       </Title>
       <p className="text-sm text-center text-lightColor/80 font-medium max-w-[480px]">
         Find everything you need to look and feel your best, and shop the latest
